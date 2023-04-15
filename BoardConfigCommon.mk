a3c4a5083a727f36bf75d439e467716fe98b4e33
DarkAngelGR <asavvo01@gmail.com>
2023-04-15 19:20:43 +0000

    temp
    Signed-off-by: DarkAngelGR <asavvo01@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -37,13 +37,13 @@ DEXPREOPT_GENERATE_APEX_IMAGE := true
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-2a-dotprod
 TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_VARIANT := kryo585
+TARGET_CPU_VARIANT := kryo785
 
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv8-2a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := kryo585
+TARGET_2ND_CPU_VARIANT := kryo385
 
 # Audio
 AUDIO_FEATURE_ENABLED_AHAL_EXT := false
@@ -226,7 +226,7 @@ endif
 # VINTF
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/framework_compatibility_matrix.xml \
-    vendor/aosp/config/device_framework_matrix.xml
+    vendor/evolution/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE += $(COMMON_PATH)/compatibility_matrix.xml
 ODM_MANIFEST_SKUS += nfc
